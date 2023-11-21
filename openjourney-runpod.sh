36688348656f247065fb34f64bb5d6fac151bd36
milankmezic <150547057+milankmezic@users.noreply.github.com>
2023-11-21 15:37:17 -0500

    Create openjourney-runpod.sh

---
+++ openjourney-runpod.sh
@@ -0,0 +1,19 @@
+curl --request POST \
+     --url https://api.runpod.ai/v2/sd-openjourney/runsync \
+     --header 'accept: application/json' \
+     --header 'authorization: [apiKey]' \
+     --header 'content-type: application/json' \
+     --data '
+{
+  "input": {
+    "prompt": "[prompt]",
+    "width": 512,
+    "height": 512,
+    "guidance_scale": 7.5,
+    "num_inference_steps": 50,
+    "num_outputs": 1,
+    "prompt_strength": 0.8,
+    "scheduler": "K-LMS"
+  }
+}
+'
