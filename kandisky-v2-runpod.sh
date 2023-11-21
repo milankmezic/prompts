f9695cc7e8739dff221125148ea4ea7e8b55acbe
milankmezic <150547057+milankmezic@users.noreply.github.com>
2023-11-21 15:33:54 -0500

    Update kandisky-v2-runpod.sh

--- kandisky-v2-runpod.sh
+++ kandisky-v2-runpod.sh
@@ -6,8 +6,8 @@ curl --request POST \
      --data '
 {
   "input": {
-    "prompt": "Einstein in space around the logarithm scheme",
-    "negative_prompt": "disfigured mouth, disfigured teeth, half head, half face, blury, side looking, old, wrinkle, child, no face, pencil, full body, sharp, far away, overlapping, duplication, nude, disfigured, kitsch, oversaturated, grain, low-res, Deformed, blurry, bad anatomy, poorly drawn face, mutation, mutated, extra limb, ugly, poorly drawn hands, missing limb, blurry, floating limbs, disconnected limbs, malformed hands, blur, out of focus, long body, disgusting, poorly drawn, childish, mutilated, mangled, surreal, out of frame, duplicate, 2 faces",
+    "prompt": "[prompt]",
+    "negative_prompt": "disfigured mouth, disfigured teeth, half head, half face, blurry, side looking, old, wrinkle, child, no face, pencil, full body, sharp, far away, overlapping, duplication, nude, disfigured, kitsch, oversaturated, grain, low-res, Deformed, blurry, bad anatomy, poorly drawn face, mutation, mutated, extra limb, ugly, poorly drawn hands, missing limb, blurry, floating limbs, disconnected limbs, malformed hands, blur, out of focus, long body, disgusting, poorly drawn, childish, mutilated, mangled, surreal, out of frame, duplicate, 2 faces",
     "num_steps": 100,
     "guidance_scale": 4,
     "h": 768,
