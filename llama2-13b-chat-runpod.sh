76d977d505dafc9fc533ff7621f41abbcf18fb1d
milankmezic <150547057+milankmezic@users.noreply.github.com>
2023-11-21 16:00:29 -0500

    Create llama2-13b-chat-runpod.sh

---
+++ llama2-13b-chat-runpod.sh
@@ -0,0 +1,23 @@
+curl -v --request POST \
+     --url https://api.runpod.ai/v2/llama2-13b-chat/runsync \
+     --header 'accept: application/json' \
+     --header 'authorization: [apiKey]' \
+     --header 'content-type: application/json' \
+     --data '
+{
+     "input": {
+       "prompt": "[prompt]",
+       "sampling_params": {
+           "max_tokens": 100,
+           "n": 1,
+           "presence_penalty": 0.2,
+           "frequency_penalty": 0.7,
+           "temperature": 0.3,
+       }
+     },
+  "outputpath": "image",
+  "title": "LLama2 13B Chat",
+  "description": "Runs on Runpod.ai",
+  "placeholder": "Enter prompt to generate text"
+}
+'
