d991a4212fd003bc4c29c67bf535b7cbb0c20248
milankmezic <150547057+milankmezic@users.noreply.github.com>
2023-11-21 09:23:25 -0500

    Create kandisky-v2-runpod.sh

---
+++ kandisky-v2-runpod.sh
@@ -0,0 +1,22 @@
+curl --request POST \
+     --url https://api.runpod.ai/v2/kandinsky-v2/runsync \
+     --header "accept: application/json" \
+     --header "authorization: [apiKey]" \
+     --header "content-type: application/json" \
+     --data '
+{
+  "input": {
+    "prompt": "Einstein in space around the logarithm scheme",
+    "negative_prompt": "disfigured mouth, disfigured teeth, half head, half face, blury, side looking, old, wrinkle, child, no face, pencil, full body, sharp, far away, overlapping, duplication, nude, disfigured, kitsch, oversaturated, grain, low-res, Deformed, blurry, bad anatomy, poorly drawn face, mutation, mutated, extra limb, ugly, poorly drawn hands, missing limb, blurry, floating limbs, disconnected limbs, malformed hands, blur, out of focus, long body, disgusting, poorly drawn, childish, mutilated, mangled, surreal, out of frame, duplicate, 2 faces",
+    "num_steps": 100,
+    "guidance_scale": 4,
+    "h": 768,
+    "w": 768,
+    "sampler": "ddim",
+    "prior_cf_scale": 4,
+    "prior_steps": "5",
+    "num_images": 1,
+    "seed": -1
+  }
+}
+'
