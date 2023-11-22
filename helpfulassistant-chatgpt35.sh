29d282fea6b89e49c7d7d7f66ad2b1f515284ad7
milankmezic <150547057+milankmezic@users.noreply.github.com>
2023-11-21 21:13:29 -0500

    Update helpfulassistant-chatgpt35.sh

--- helpfulassistant-chatgpt35.sh
+++ helpfulassistant-chatgpt35.sh
@@ -8,5 +8,16 @@ curl --request POST \
        "messages": [
          {"role": "system", "content": "You are a helpful assistant."},
          {"role": "user", "content": "[prompt]"}
-       ]
+       ],
+       "options": {
+         "temperature": 0.7,
+         "max_tokens": 2000,
+         "stop": ["\n", "User:"],
+         "frequency_penalty": 0.5,
+         "presence_penalty": 0.2
+         },
+          "outputpath": "text",
+       "title": "ChatGPT 3.5 Turbo",
+       "description": "Runs on Open.ai",
+       "placeholder": "Enter prompt to generate text"
      }'
