289085337d15efe88e02273d81adc61b674ca311
milankmezic <150547057+milankmezic@users.noreply.github.com>
2023-11-21 20:54:40 -0500

    Update helpfulassistant-chatgpt35.sh

--- helpfulassistant-chatgpt35.sh
+++ helpfulassistant-chatgpt35.sh
@@ -1,8 +1,12 @@
-curl -X POST -H "Authorization: Bearer [apiKey]" -H "Content-Type: application/json" -d '{
-  "model": "gpt-3.5-turbo",
-  "messages": [
-    {"role": "system", "content": "You are a helpful assistant."},
-    {"role": "user", "content": "What's the weather like today?"},
-    {"role": "assistant", "content": ""}
-  ]
-}' "https://api.openai.com/v1/engines/gpt-3.5-turbo/completions"
+curl --request POST \
+     --url https://api.openai.com/v1/engines/gpt-3.5-turbo/completions \
+     --header 'accept: application/json' \
+     --header 'authorization: [apiKey]' \
+     --header 'content-type: application/json' \
+     --data '{
+       "model": "gpt-3.5-turbo",
+       "messages": [
+         {"role": "system", "content": "You are a helpful assistant."},
+         {"role": "user", "content": "[prompt]"}
+       ]
+     }'
