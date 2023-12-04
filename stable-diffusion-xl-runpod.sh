6e39563a88b6d40e1dfb1ff3365536ef94a572d6
milankmezic <150547057+milankmezic@users.noreply.github.com>
2023-12-04 00:33:06 -0500

    Update stable-diffusion-xl-runpod.sh

--- stable-diffusion-xl-runpod.sh
+++ stable-diffusion-xl-runpod.sh
@@ -1,7 +1,7 @@
 curl --request POST \
      --url https://api.runpod.ai/v2/sdxl/runsync \
      --header 'accept: application/json' \
-     --header 'authorization: [apiKey]' \
+     --header 'authorization: [apiKeyRunPod]' \
      --header 'content-type: application/json' \
      --data '
 {
