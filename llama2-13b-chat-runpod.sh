5a6d47c1c213c2ca54114542ebf65691dc0bb51f
milankmezic <150547057+milankmezic@users.noreply.github.com>
2023-12-04 00:33:41 -0500

    Update llama2-13b-chat-runpod.sh

--- llama2-13b-chat-runpod.sh
+++ llama2-13b-chat-runpod.sh
@@ -1,7 +1,7 @@
 curl --request POST \
      --url https://api.runpod.ai/v2/llama2-13b-chat/runsync \
      --header 'accept: application/json' \
-     --header 'authorization: [apiKey]' \
+     --header 'authorization: [apiKeyRunPod]' \
      --header 'content-type: application/json' \
      --data '
 {
