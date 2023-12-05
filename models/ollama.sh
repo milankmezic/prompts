92ba4b9f7a6f48076e7412f6c7d2b78bdb5092e2
milankmezic <150547057+milankmezic@users.noreply.github.com>
2023-12-05 14:21:13 -0500

    Update ollama.sh

--- models/ollama.sh
+++ models/ollama.sh
@@ -1,8 +1,5 @@
 curl --request POST \
      --url http://127.0.0.1:11434/api/generate \
-     --header 'accept: application/json' \
-     --header 'authorization: Bearer [apiKeyChatGPT]' \
-     --header 'content-type: application/json' \
      --data '{
        "model": "llama2",
        "prompt" :"[prompt]"
