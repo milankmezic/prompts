daa260d7abdc7942c0608c462b79e06ed9f32c45
milankmezic <150547057+milankmezic@users.noreply.github.com>
2023-12-04 23:37:26 -0500

    Update azureopenai.sh

--- models/azureopenai.sh
+++ models/azureopenai.sh
@@ -1,9 +1,12 @@
 
-curl "https://acgaiinstance.openai.azure.com/openai/deployments/acg-16k/chat/completions?api-version=2023-07-01-preview" \
+curl "https://acgaiinstance.openai.azure.com/openai/deployments/gpt-35-turbo/chat/completions?api-version=2023-07-01-preview" \
   -H "Content-Type: application/json" \
   -H "api-key: [apiKeyAzure]" \
   -d "{
-  \"messages\": [{\"role\":\"system\",\"content\":\"You are an AI assistant that helps people find information. Answer this question: [prompt]\"}],
+   \"messages\": [
+    {\"role\":\"system\",\"content\":\"You are a helpful assistant.\"},
+    {\"role\":\"user\",\"content\":\"[prompt]\"}
+  ],
   \"max_tokens\": 2000,
   \"temperature\": 0.7,
   \"frequency_penalty\": 0,
