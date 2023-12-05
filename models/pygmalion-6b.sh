a93f7c9242a42a144da62f1bff988bdc677708d0
milankmezic <150547057+milankmezic@users.noreply.github.com>
2023-12-05 11:10:25 -0500

    Update pygmalion-6b.sh

--- models/pygmalion-6b.sh
+++ models/pygmalion-6b.sh
@@ -1,7 +1,7 @@
 curl --request POST \
      --url https://api.runpod.ai/v2/pygmalion-6b/runsync \
      --header 'accept: application/json' \
-     --header 'authorization: [apiKeyRunPod] \
+     --header 'authorization: [apiKeyRunPod]' \
      --header 'content-type: application/json' \
     --data '
      {
