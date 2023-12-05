7de3a8b3586d20eff8eb0d54bc35021b869ead55
milankmezic <150547057+milankmezic@users.noreply.github.com>
2023-12-05 14:01:37 -0500

    Update ollama.sh

--- models/ollama.sh
+++ models/ollama.sh
@@ -1,5 +1,6 @@
-curl http://127.0.0.1:11434/api/generate \
--d '{
-  "model": "llama2",
-  "prompt":"[prompt]"
-}'
+curl --request POST \
+     --url http://127.0.0.1:11434/api/generate \
+  -d '{
+    "model": "llama2",
+    "prompt":"[prompt]"
+  }'
